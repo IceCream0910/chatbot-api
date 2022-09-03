26ca2044739bf5205ed1303ede603e76fc913f01
Icecream0910 <taein2370@naver.com>
2022-09-03 20:52:22 +0900

    INIT

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+var request = require("request");
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+const cors = require('cors');
+global.document = document;
+var $ = jQuery = require('jquery')(window);
+const moment = require("moment");
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get("/:text", (req, res_api) => {
+  var text = req.params.text.toString();
+  console.log(text);
+  var data = {
+    "request": {
+      "query": text
+    }
+  };
+  var options = {
+    url: "https://builder.pingpong.us/api/builder/63133aece4b0793ad020a1aa/integration/v0.2/custom/5d234ee2134aef2123",
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Authorization": "Basic a2V5OjMwNDU2YzQ1OTg0NjQ4N2Y5MzdlODM1YWE3ZjZkYTdm",
+      "Content-Type": "application/json"
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      var result = data.response.replies[0].text;
+      res_api.send(result);
+    }
+  });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
