15889e3839ca6006a6eb8d6e549aab8bd892d5e6
Icecream0910 <taein2370@naver.com>
2023-01-02 00:12:05 +0900

    ADD: kakao kogpt api

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+const REST_API_KEY = process.env.KAKAO_REST_API_KEY;
 
+// pingpong chat
 app.post("/api", (req, res_api) => {
   const { prompt, sessionId } = req.body;
   var data = {
@@ -40,6 +42,39 @@ app.post("/api", (req, res_api) => {
       res_api.send(result);
     }
   });
+});
+
+// kakao kogpt
+app.post("/koGPT", async (req, res) => {
+  const { prompt, max_tokens, temperature, top_p, n } = req.body;
+  console.log(req.body);
+  if (prompt) {
+    var headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `KakaoAK ${REST_API_KEY}`
+    };
+
+    var dataString = {
+      "prompt": prompt,
+      "max_tokens": max_tokens || 10,
+      "temperature": temperature || 1,
+      "top_p": top_p || 1,
+      "n": n || 1
+    };
+
+    var options = {
+      url: 'https://api.kakaobrain.com/v1/inference/kogpt/generation',
+      headers: headers,
+      body: JSON.stringify(dataString)
+    };
+
+    request.post(options, function (error, response, body) {
+      console.log('responseCode = ' + response.statusCode);
+      res.send(body);
+    });
+  } else {
+    res.json({ error: '프롬프트를 입력해주세요.' });
+  }
 });
 
 
